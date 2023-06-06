7836207949bf5db200b64e760a409fab25d7e068
107124 <kredfox4@gmail.com>
2023-06-06 19:46:11 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    direction = request.args.get('direction', '')
+    return render_template('index.html', direction=direction)
+
+@app.route('/up')
+def up():
+    direction = 'up'
+    return render_template('index.html', direction=direction)
+
+@app.route('/down')
+def down():
+    direction = 'down'
+    return render_template('index.html', direction=direction)
+
+@app.route('/left')
+def left():
+    direction = 'left'
+    return render_template('index.html', direction=direction)
+
+@app.route('/right')
+def right():
+    direction = 'right'
+    return render_template('index.html', direction=direction)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(host='192.168.0.100', port=5000)
+    # Nathan, we will need to IP address to the raspi
